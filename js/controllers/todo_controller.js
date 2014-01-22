d2c084c92f447d31ca5ece777247c1c8c59a8585
Edipo Luis Federle <edipofederle@gmail.com>
2014-01-21 23:18:13 -0200

    Finalizado. Emberjs é legal :D

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -13,6 +13,11 @@ Todos.TodoController = Ember.ObjectController.extend({
       }
     }
   },
+  removeTodo: function(){
+    var todo = this.get('model');
+    todo.deleteRecord();
+    todo.save();
+  },
 
   isEditing: false,
 
